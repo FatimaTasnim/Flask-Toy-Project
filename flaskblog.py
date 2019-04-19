cad4ca9cda4275bb3a0d63d1fa1dba06106f43d3
FatimaTasnim <tasnim.roujat@gmail.com>
2019-04-19 17:49:41 +0600

    first commit

---
+++ flaskblog.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'No One',
+        'title': 'No title required',
+        'content': 'Nothing',
+        'date_posted': 'April 20, 2018'
+    },
+
+    {
+        'author': 'Arya Stark',
+        'title': 'No title required',
+        'content': 'Nothing',
+        'date_posted': 'April 21, 2018'
+    }
+]
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = posts)
+@app.route("/about")
+def about():
+    return render_template('about.html')
+if __name__ == '__main__':
+    app.run(debug=True)
