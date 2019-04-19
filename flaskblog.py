114772d09696bdfccc3f224c9c97bbf975040020
FatimaTasnim <tasnim.roujat@gmail.com>
2019-04-19 19:14:02 +0600

    nan

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
@@ -22,6 +22,6 @@ def home():
     return render_template('home.html', posts = posts)
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title = 'about')
 if __name__ == '__main__':
     app.run(debug=True)
